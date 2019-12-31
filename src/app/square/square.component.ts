1d33846bc0f97134d9b8f337a8042755eb880748
Reymark Torres <reymark.torres08.dev@gmail.com>
2019-12-31 20:15:32 +0800

    Create a 'dumb' square component

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <button>{{ value }}</button>
+  `,
+  styles: []
+})
+export class SquareComponent {
+  @Input() value: 'X' | 'O';
+}
