08150012cc4623aaf1580db50f1cab68efacc16f
Reymark Torres <reymark.torres08.dev@gmail.com>
2019-12-31 20:17:31 +0800

    Create initial 'smart' board component

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
+})
+export class BoardComponent implements OnInit {
+  squares: any;
+  xIsNext: boolean;
+  winner: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
